661cd9fcf208a646c7331a4d6ebabc12b4bfec12
like <likeforeverx@163.com>
2016-08-30 07:11:39 +0800

    增加了member function的SFINAE的测试

--- c++/template/template.cpp
+++ c++/template/template.cpp
@@ -9,63 +9,14 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include "SFINAE.h"
 
 using namespace std;
 
-
-
-template<class _pg>
-class ProgressBase
-{
-public:
-        void print(_pg pg){
-                cout << pg << endl;
-                m_pg = pg;
-        }
-protected:
-        _pg m_pg;
-};
-
-template<class _pg, class _placeHolder = void>
-class Progr : public ProgressBase<_pg>
-{
-public:
-private:
-}; 
-
-template<class _pg>
-class Progr<_pg, typename std::enable_if< std::is_integral<_pg>::value || std::is_floating_point<_pg>::value  >::type> : public ProgressBase<_pg>
-{
-public:
-        void inc(){
-                m_pg++;
-        }
-
-        void justPrint(){
-                cout << m_pg << endl;
-        }
-private:
-};
-
-
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-        //1. 通过继承的方式，让类特化时不用copy所有代码
-        //2. 利用enable_if来为interge和float型增加新函数
-        //3. 使用了偏特化和点位的模板类型形参来达到此目标
-        Progr<string> sp;
-        sp.print("x");
-
-        Progr<int> sp2;
-        sp2.print(23);
-        sp2.inc();
-        sp2.justPrint();
-
-        Progr<float> sp3;
-        sp3.print(23.4f);
-        sp3.inc();
-        sp3.justPrint();
+        TestSfinae::testClass();
+        TestSfinae::testMemberFunction();
 
         return 0;
 }
